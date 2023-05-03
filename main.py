22773c17aeace5776f16403b2af1504c80b53f8a
drewbear82 <andrewkentjohnston@gmail.com>
2023-05-02 23:23:59 -0600

    finished main.py

--- main.py
+++ main.py
@@ -26,3 +26,18 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+import random
+list = [rock, paper, scissors]
+player_choice = int(input("What do  you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
+computer_choice = random.randint(0, 2)
+
+print(list[player_choice])
+print(f"Copmuter chose:\n{list[computer_choice]}\n")
+
+if player_choice == computer_choice:
+    print("Tie!")
+elif player_choice == computer_choice + 1\
+    or player_choice == 0 and computer_choice == 2:
+    print("You win!")
+else:
+    print("You lose!")
